0b8ca9f6d9ceeaf4564f9e9cc01d69328c8075ea
郝紫俊 <hzjun20021001@163.com>
2023-09-01 16:48:38 +0800

    reggie版本1

--- reggie_take_out/src/main/java/com/hzj/controller/DishController.java
+++ reggie_take_out/src/main/java/com/hzj/controller/DishController.java
@@ -4,13 +4,19 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hzj.common.R;
 import com.hzj.dto.DishDto;
+import com.hzj.pojo.Category;
 import com.hzj.pojo.Dish;
 import com.hzj.pojo.DishFlavor;
+import com.hzj.service.CategoryService;
 import com.hzj.service.DishFlavorService;
 import com.hzj.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * PackageName :com.hzj.controller
@@ -31,6 +37,8 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    @Autowired
+    private CategoryService categoryService;
     @PostMapping
     public R<String> save(@RequestBody DishDto dto){
         log.info(dto.toString());
@@ -42,6 +50,8 @@ public class DishController {
     public R<Page> page(int page ,int pageSize,String name){
         //构造分页器
         Page<Dish> pageInfo =new Page<>(page,pageSize);
+
+        Page<DishDto> dishDtoPage=new Page<>(page,pageSize);
         //条件构造器
         LambdaQueryWrapper<Dish> queryWrapper=new LambdaQueryWrapper();
 
@@ -52,7 +62,25 @@ public class DishController {
 
         dishService.page(pageInfo,queryWrapper);
 
-        return R.success(pageInfo);
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,dishDtoPage,"records");
+
+        List<Dish> records = pageInfo.getRecords();
+        List<DishDto> list=records.stream().map((item)->{
+            DishDto dto=new DishDto();
+            BeanUtils.copyProperties(item,dto);
+            Long categoryId = item.getCategoryId();//分类ID
+            //根据id查询分类信息
+            Category category = categoryService.getById(categoryId);
+            if (category!=null){
+                String categoryName = category.getName();
+                dto.setCategoryName(categoryName);
+            }
+
+            return dto;
+        }).collect(Collectors.toList());
+        dishDtoPage.setRecords(list);
+        return R.success(dishDtoPage);
 
     }
 }
